889971c9918916adf3ac74781e15b7910f190aaf
Alif <31809831+Maniizzle@users.noreply.github.com>
2020-08-04 09:01:42 +0100

    Update Seed.cs

--- QuizzyAPI/QuizzyAPI/Infrastructure/Data/Seed.cs
+++ QuizzyAPI/QuizzyAPI/Infrastructure/Data/Seed.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace QuizzyAPI.Data
+namespace QuizzyAPI.Infrastructure.Data
 {
     public class Seed
     {
